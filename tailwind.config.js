0cc72b60ff2901ac3e1755d70c9669b74960c4f6
Yujin <yujinyll9148@gmail.com>
2024-12-28 17:55:31 +0900

    Design: grayScale 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          white: "#FFFFFF",
+          50: "#F4F8FA",
+          100: "#CDD9E4",
+          200: "#90A2B7",
+          300: "#768797",
+          400: "#56687A",
+          500: "#41505B",
+          black: "#1E272E",
+        },
+      },
+    },
   },
   plugins: [],
 };
