e41073b5db100a656160503c1cf186d26b32f1c0
Yujin <yujinyll9148@gmail.com>
2025-01-03 14:18:25 +0900

    input 입력 안되는 거 수정

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 interface InputProps {
-  value?: string;
+  initialValue?: string;
   placeholder?: string;
 }
 
-function Input({ value = "", placeholder = "" }: InputProps) {
+function Input({ initialValue = "", placeholder = "" }: InputProps) {
+  const [value, setValue] = useState(initialValue);
   return (
     <input
       type="text"
       value={value}
+      onChange={(e) => setValue(e.target.value)}
       placeholder={placeholder}
       className="w-full border border-gray-100 text-base px-4 py-3 rounded-lg placeholder:text-gray-100"
     />
