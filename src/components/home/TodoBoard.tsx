32220b4b9ee297841f16b5d4632ff16bb5a3ddbc
chichoc <chichoc.dev@gmail.com>
2025-01-03 16:18:30 +0900

    style: 💄 투두보드 간격 조정

--- src/components/home/TodoBoard.tsx
+++ src/components/home/TodoBoard.tsx
@@ -6,7 +6,7 @@ function TodoBoard() {
   const remainingTodos = useTodoStore((state) => state.remainingTodos);
 
   return (
-    <div className="mt-12 w-96 rounded-2xl bg-white p-6 drop-shadow-xl">
+    <div className="mt-3 w-96 rounded-2xl bg-white p-6 drop-shadow-xl">
       <h2 className="mb-2 text-xl font-bold">완료한 할 일</h2>
       <BoardList todos={completeTodos} />
       <hr className="my-4 border-gray-50" />
