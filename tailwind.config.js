939713084c83d23baf063da4c667cdffbf02c58c
chichoc <chichoc.dev@gmail.com>
2025-01-03 16:30:48 +0900

    style: 💄 suit 폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         gray: {
-          white: "#FFFFFF",
-          50: "#F4F8FA",
-          100: "#CDD9E4",
-          200: "#90A2B7",
-          300: "#768797",
-          400: "#56687A",
-          500: "#41505B",
-          black: "#1E272E",
+          white: '#FFFFFF',
+          50: '#F4F8FA',
+          100: '#CDD9E4',
+          200: '#90A2B7',
+          300: '#768797',
+          400: '#56687A',
+          500: '#41505B',
+          black: '#1E272E',
         },
       },
     },
