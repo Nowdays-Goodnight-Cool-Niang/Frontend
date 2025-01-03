5d1aa9c75ee563cc6e54430b8ae74b1b220ea7e3
chichoc <chichoc.dev@gmail.com>
2025-01-03 14:16:54 +0900

    perf: ⚡️ TodoForm 재수정

--- src/components/todo/TodoForm.tsx
+++ src/components/todo/TodoForm.tsx
@@ -1,11 +1,11 @@
-import TodoAddButton from './TodoAddButton';
-import TodoInput from './TodoInput';
+import Button from '../common/Button';
+import Input from '../common/Input';
 
 function TodoForm() {
   return (
     <div className='flex flex-col justify-center gap-2.5'>
-      <TodoInput />
-      <TodoAddButton />
+      <Input placeholder='할 일을 입력해 주세요' />
+      <Button text={'추가하기'} onClick={() => {}} />
     </div>
   );
 }
